8ddfef58d4a57f0efbfb4c53f920b1c82d2e49d8
gnewton <glen.newton@gmail.com>
2019-03-23 07:13:53 -0400

    Done moving attachment content to XmlContent

--- src/main/java/ca/gnewton/pst2json/XmlAttachment.java
+++ src/main/java/ca/gnewton/pst2json/XmlAttachment.java
@@ -6,91 +6,22 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name="attachment")
 public class XmlAttachment{
-    private String filename;
-    private String mime;
-    private String sha256Hex;
-    private String attachment_content_disposition;
-    private int attach_type;
-    @XmlElement
-    XmlContent newContent;
-    @XmlElement
-    XmlContent newContentTextExtracted;
-
-    String content;
-    String contentTextExtracted;
-    /**
-     * Describe size here.
-     */
-    private int size;
-
     @XmlAttribute
-    public final String getMime() {
-	return mime;
-    }
-    public final void setMime(final String mime) {
-	this.mime = mime;
-    }
-
+     String filename;
     @XmlAttribute
-    public final String getFilename() {
-	return filename;
-    }
-    public final void setFilename(final String filename) {
-	this.filename = filename;
-    }
-
+     String mime;
     @XmlAttribute
-    public final String getAttachment_content_disposition() {
-	return attachment_content_disposition;
-    }
-    public final void setAttachment_content_disposition(final String attachment_content_disposition) {
-	this.attachment_content_disposition = attachment_content_disposition;
-    }
-
+     String sha256Hex;
     @XmlAttribute
-    public final int getAttach_type() {
-	return attach_type;
-    }
-    public final void setAttach_type(final int attach_type) {
-	this.attach_type = attach_type;
-    }
-
+     String attachment_content_disposition;
+    @XmlAttribute
+     int attach_type;
     @XmlElement
-    public final String getContent() {
-	return content;
-    }
-
-    public final void setContent(final String content) {
-	this.content = content;
-    }
-
-
+    XmlContent content;
     @XmlElement
-    public final String getSha256Hex() {
-	return sha256Hex;
-    }
-
-    public final void setSha256Hex(final String sha256) {
-	this.sha256Hex = sha256;
-    }
-
-        @XmlAttribute
-    public final int getSize() {
-	return size;
-    }
-    public final void setSize(final int size) {
-	this.size = size;
-    }
-
-
-        @XmlElement
-    public final String getContentTextExtracted() {
-	return contentTextExtracted;
-    }
-
-    public final void setContentTextExtracted(final String contentTextExtracted) {
-	this.contentTextExtracted = contentTextExtracted;
-    }
+    XmlContent contentTextExtracted;
+    @XmlElement
+     int size;
 
 
 }
